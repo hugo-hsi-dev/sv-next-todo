ed2ae57cd1fa008f3d4c966a3a93ecf09f66d5fa
Hugo Hsi <hugohsidev@gmail.com>
2026-06-05 20:40:53 -0400

    Switch SvelteKit DB config to dynamic env

--- sveltekit/src/lib/server/db/index.ts
+++ sveltekit/src/lib/server/db/index.ts
@@ -1,24 +1,10 @@
-import Database from 'better-sqlite3';
 import { drizzle } from 'drizzle-orm/better-sqlite3';
-import { DATABASE_URL } from '$env/static/private';
-
+import Database from 'better-sqlite3';
 import * as schema from './schema';
+import { env } from '$env/dynamic/private';
 
-if (!DATABASE_URL) {
-	throw new Error('DATABASE_URL is required');
-}
+if (!env.DATABASE_URL) throw new Error('DATABASE_URL is not set');
 
-const sqlite = new Database(DATABASE_URL);
+const client = new Database(env.DATABASE_URL);
 
-sqlite.exec(`
-	create table if not exists todos (
-		id integer primary key autoincrement,
-		title text not null,
-		completed integer not null default 0,
-		deleted_at integer,
-		created_at integer not null default (unixepoch('subsec') * 1000),
-		updated_at integer not null default (unixepoch('subsec') * 1000)
-	);
-`);
-
-export const db = drizzle(sqlite, { schema });
+export const db = drizzle(client, { schema });
